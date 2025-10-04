6e6e48b5c1391deeb276318e2601fb9cc1a31f1d
AjRagnarok <anthonyjennisnadar@gmail.com>
2025-10-05 01:16:56 +0530

    folders for assets

--- frontend/src/assets/index_g.js
+++
@@ -1,4 +0,0 @@
-export { KaiGraphic } from './KaiGraphic';
-export { ElaraGraphic } from './ElaraGraphic';
-export { OrionGraphic } from './OrionGraphic';
-export { AethelGraphic } from './AethelGraphic';
